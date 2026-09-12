2b68b49224996226ffcdc6f6d5ca2bea860dbaa5
nagaitashouten-star <nagaita.shouten@gmail.com>
2026-09-13 03:41:01 +0900

    Cache verified kawaii character art for v0.15.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mwg-v0154-hotfix';
+const CACHE='mwg-v0154-kawaii';
 const ASSETS=[
   './',
   './index.html',
@@ -6,13 +6,13 @@ const ASSETS=[
   './v012.js',
   './assets/art-manifest.js',
   './assets/background/garden-base-dev.svg',
-  './assets/animals/rabbit/idle-temp.svg',
-  './assets/animals/cat/idle-temp.svg',
-  './assets/animals/squirrel/idle-temp.svg',
-  './assets/animals/duck/idle-temp.svg',
-  './assets/animals/hedgehog/idle-temp.svg',
-  './assets/animals/bird/idle-temp.svg',
-  './assets/animals/dog/idle-temp.svg'
+  './assets/animals/rabbit/idle-v0153.svg',
+  './assets/animals/cat/idle-v0153.svg',
+  './assets/animals/squirrel/idle-v0153.svg',
+  './assets/animals/duck/idle-v0153.svg',
+  './assets/animals/hedgehog/idle-v0153.svg',
+  './assets/animals/bird/idle-v0153.svg',
+  './assets/animals/dog/idle-v0153.svg'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
