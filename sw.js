23c12cd1eb0a809449010d586a370911f29fa28b
nagaitashouten-star <nagaita.shouten@gmail.com>
2026-09-13 02:31:16 +0900

    Activate v0.15.1 cache immediately

--- sw.js
+++ sw.js
@@ -10,6 +10,6 @@ const ASSETS=[
   './assets/animals/bird/idle-temp.svg','./assets/animals/bird/water-temp.svg','./assets/animals/bird/happy-temp.svg','./assets/animals/bird/eat-temp.svg',
   './assets/animals/dog/idle-temp.svg','./assets/animals/dog/water-temp.svg','./assets/animals/dog/happy-temp.svg','./assets/animals/dog/eat-temp.svg'
 ];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
